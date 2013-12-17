96fbe554b6c401064c57a169afdb49d602e2793a
terra <terrach@terranz.ath.cx>
2013-12-17 15:57:10 +0400

    some refactoring

--- jersey/src/main/java/ru/terra/server/constants/FilePathConstants.java
+++
@@ -1,5 +0,0 @@
-package ru.terra.server.constants;
-
-public class FilePathConstants {
-	public static final String PRODUCT_IMAGES_DIR = "/controller/images/product";
-}
---
+++ server/src/main/java/ru/terra/server/db/controllers/AbstractDBController.java
@@ -0,0 +1,39 @@
+package ru.terra.server.db.controllers;
+
+import ru.terra.server.config.Config;
+
+import java.util.List;
+
+/**
+ * User: Vadim Korostelev
+ * Date: 13.12.13
+ * Time: 17:57
+ */
+public abstract class AbstractDBController<Entity> {
+    protected Class<Entity> entityClass;
+    protected Config config = Config.getConfig();
+
+    public AbstractDBController(Class<Entity> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    public abstract void create(Entity entity) throws Exception;
+
+    public abstract void delete(Integer id) throws Exception;
+
+    public abstract void update(Entity entity) throws Exception;
+
+    public void setEntityClass(Class<Entity> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    public Class getEntityClass() {
+        return entityClass;
+    }
+
+    public abstract List<Entity> list(boolean all, int page, int perpage) throws Exception;
+
+    public abstract int count();
+
+    public abstract Entity get(Integer id) throws Exception;
+}
